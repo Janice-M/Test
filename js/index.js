7f55c81e9a485ae21fcb1c9c20dbd15528346c1c
Janice-M <janiceink001@gmail.com>
2019-11-14 02:30:41 +0300

    getting my api key

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-console.log ('hakuna matata');
+console.log("hakuna matata");
+console.log("69");
 
-function weatherBalloon( cityID ) {
-    var key = '{bea6972840a72f62be17686103c7aed4}';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
-    .then(function(resp) { return resp.json() }) // Convert data to json
-    .then(function(data) {
-      console.log(data);
-    })
-    .catch(function() {
-      // catch any errors
-    });
-  }
-  
-  window.onload = function() {
-    weatherBallon( 6167865 );
-  }
+let request = require("request");
+let apiKey = 'bea6972840a72f62be17686103c7aed4';
+let city = 'Nairobi';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function(err, response, body) {
+    if (err) {
+    console.log("error:", error);
+    } else {
+    console.log("body:", body);
+    }
+});
+
+
