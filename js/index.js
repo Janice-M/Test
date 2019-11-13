7d5a2660c8fa0f87006b41754c20b47b66d1bcb9
Janice-M <janiceink001@gmail.com>
2019-11-14 01:53:14 +0300

    getting my api key

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log (bea6972840a72f62be17686103c7aed4)
+console.log ('hakuna matata');
+
+function weatherBalloon( cityID ) {
+    var key = '{bea6972840a72f62be17686103c7aed4}';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBallon( 6167865 );
+  }
