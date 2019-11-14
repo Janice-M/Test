c4181194ee19c3efa60a5787092689d0210df9bb
Janice-M <janiceink001@gmail.com>
2019-11-14 09:33:50 +0300

    showing the city

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
-console.log("hakuna matata");
-console.log("69");
+$(document).ready(function(){
 
-let request = require("request");
-let apiKey = 'bea6972840a72f62be17686103c7aed4';
-let city = 'Nairobi';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+	$('#submitWeather').click(function(){
 
-request(url, function(err, response, body) {
-    if (err) {
-    console.log("error:", error);
-    } else {
-    console.log("body:", body);
-    }
+		var city = $("#city").val();
+
+		if (city != ''){
+
+			$.ajax({
+
+				url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&appid=bea6972840a72f62be17686103c7aed4",
+				type: "GET",
+				dataType: "jsonp",
+				success: function(data){
+					var widget = show(data);
+
+					$("#show").html(widget);
+
+					$("#city").val('');
+				}
+			});
+
+		}else{
+			$("#error").html('Field cannot be empty');
+		}
+	});
 });
 
-
+function show(data) {
+	return "<h3 style=  ><strong>Weather</strong>: "+ data.main.temp +"</h3>"
+}
